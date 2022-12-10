a6a948d1e18ec4e6764b72a01b9978269d9cc156
NguyenTu1601 <nguyenvantu.qng1234@gmail.com>
2022-12-11 04:56:51 +0700

    feat:loading when load

--- review-restaurant-client/src/components/restaurant/ListSearch.jsx
+++ review-restaurant-client/src/components/restaurant/ListSearch.jsx
@@ -9,18 +9,23 @@ import './listSearchPage.css';
 const ListSearch = (props) => {
   const [listRestaurant, setListRestaurant] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [regionApi, setRegionApi] = useState([])
+  const [purposesApi, setPurposesApi] = useState([])
+  const [benefitsApi, setBenefitsApi] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    console.log('search');
-    console.log(props.region);
-    console.log(props.purposes);
-    console.log(props.benefits);
-    console.log('search');
-  }, []);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+    setRegionApi(props.region)
+    setPurposesApi(props.purposes.map(Number))
+    setBenefitsApi(props.benefits.map(Number))
+  }, [props.region, props.purposes, props.benefits]);
+
 
   const onChangePage = (page) => {
-    // console.log(page);
-    // window.scrollTo(0, 0);
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
@@ -28,126 +33,108 @@ const ListSearch = (props) => {
     setCurrentPage(page);
   };
 
-  // const [{ data, loading, error }, refetch] = useAxios({
-  //   url: 'http://localhost:5000/users/searchManyFields',
-  //   method: 'GET',
-  //   body: {
-  //     regions: props.region,
-  //     purposes: props.purposes,
-  //     benefits: props.benefits,
-  //     minPrice: props.minPrice,
-  //     maxPrice: props.maxPrice,
-  //   },
-  // });
-  // // useEffect(() => {
-  // //   refetch({ url: '' });
-  // // }, []);
-  // console.log('a');
-  // console.log(data);
-  // console.log('a');
-
-  const purposes1 = props.purposes.map(Number);
-  console.log('ee', purposes1);
   useEffect(() => {
-    const getListReataurantByField = async () => {
+    const getListRestaurantByField = async () => {
       try {
+        setLoading(true)
         const res = await axios.post('http://localhost:5000/users/searchManyFields', {
           key: '',
-          regions: props.region,
-          purposes: props.purposes,
-          benefits: props.benefits,
+          regions: regionApi,
+          purposes: purposesApi,
+          benefits: benefitsApi,
           minPrice: props.minPrice * 10000,
           maxPrice: props.maxPrice * 10000,
         });
-        setListRestaurant(res.data.users);
+        setListRestaurant(res.data.users.filter(item => item.role === "RESTAURANT"));
+        setLoading(false)
       } catch (err) {
         console.log(err);
       }
     };
-    getListReataurantByField();
+    getListRestaurantByField();
   }, [props]);
 
-  // console.log(listRestaurant);
-  // console.log(listRestaurant[5]?.infoRestaurant.images[0].url);
-  // console.log(listRestaurant.length);
-
-  console.log();
   return (
     <div>
-      <div class="listing-products">
-        {listRestaurant?.map((item, idx) => {
-          if (idx >= 10 * (currentPage - 1) && idx <= Math.min(listRestaurant?.length, 10 * (currentPage - 1) + 9)) {
-            return (
-              <div class="listing-product">
-                {/* {console.log(idx)} */}
-                <div class="product-thumb">
-                  <img src={listRestaurant[idx]?.infoRestaurant?.images[0]?.url} alt="" />
-                </div>
-                <div class="product-info">
-                  <h3>
-                    <a href="restaurant-details.html" title="">
-                      Vegetarian Fresh life
-                    </a>
-                  </h3>
-                  <ul class="rating">
-                    <li>
-                      <i class="fa fa-star"></i>
-                    </li>
-                    <li>
-                      <i class="fa fa-star"></i>
-                    </li>
-                    <li>
-                      <i class="fa fa-star"></i>
-                    </li>
-                    <li>
-                      <i class="fa fa-star"></i>
-                    </li>
-                    <li>
-                      <i class="fa fa-star"></i>
-                    </li>
-                  </ul>
-                  <p>
-                    Salads, soups, sneaks, burgers, drinks, seafood, grill, fish, vegetarian menu, wine, hot meals,
-                    steaks...
-                  </p>
-                  <ul class="btm">
-                    <li>
-                      <a href="#" title="">
-                        Restaurant
+      {loading ? (<div className='loading-container'><div class="loadingio-spinner-reload-0otlv348doe"><div class="ldio-84ztw36yse6">
+        <div><div></div><div></div><div></div></div>
+      </div></div></div>) : (<div>
+        <div class="listing-products">
+          {listRestaurant?.map((item, idx) => {
+            if (idx >= 10 * (currentPage - 1) && idx <= Math.min(listRestaurant?.length, 10 * (currentPage - 1) + 9)) {
+              return (
+                <div class="listing-product">
+                  {/* {console.log(idx)} */}
+                  <div class="product-thumb">
+                    <img src={listRestaurant[idx]?.infoRestaurant?.images[0]?.url} alt="" />
+                  </div>
+                  <div class="product-info">
+                    <h3>
+                      <a href="restaurant-details.html" title="">
+                        Vegetarian Fresh life
                       </a>
-                    </li>
-                    <li>
-                      <span>
-                        <b class="statuss"></b>Open now
-                      </span>
-                    </li>
-                  </ul>
-                  <a href="restaurant-details.html" title="" class="view-menu">
-                    View Menu <i class="fa fa-long-arrow-alt-right"></i>
-                  </a>
-                  <div class="clearfix"></div>
+                    </h3>
+                    <ul class="rating">
+                      <li>
+                        <i class="fa fa-star"></i>
+                      </li>
+                      <li>
+                        <i class="fa fa-star"></i>
+                      </li>
+                      <li>
+                        <i class="fa fa-star"></i>
+                      </li>
+                      <li>
+                        <i class="fa fa-star"></i>
+                      </li>
+                      <li>
+                        <i class="fa fa-star"></i>
+                      </li>
+                    </ul>
+                    <p>
+                      Salads, soups, sneaks, burgers, drinks, seafood, grill, fish, vegetarian menu, wine, hot meals,
+                      steaks...
+                    </p>
+                    <ul class="btm">
+                      <li>
+                        <a href="#" title="">
+                          Restaurant
+                        </a>
+                      </li>
+                      <li>
+                        <span>
+                          <b class="statuss"></b>Open now
+                        </span>
+                      </li>
+                    </ul>
+                    <a href="restaurant-details.html" title="" class="view-menu">
+                      View Menu <i class="fa fa-long-arrow-alt-right"></i>
+                    </a>
+                    <div class="clearfix"></div>
+                  </div>
                 </div>
-              </div>
-            );
-          }
-        })}
-      </div>
+              );
+            }
+          })}
+        </div>
 
-      <div class="load-more mt-40 text-center">
-        {/* <a href="#" title="" class="btn-default">
+        <div class="load-more mt-40 text-center">
+          {/* <a href="#" title="" class="btn-default">
           Load More <span></span>
         </a> */}
-        <Pagination
-          style={{ color: 'yellow' }}
-          defaultCurrent={1}
-          current={currentPage}
-          total={listRestaurant?.length}
-          onChange={onChangePage}
-          pageSize={10}
-          showLessItems={false}
-          showSizeChanger={false}
-        />
-      </div>
+          <Pagination
+            style={{ color: 'yellow' }}
+            defaultCurrent={1}
+            current={currentPage}
+            total={listRestaurant?.length}
+            onChange={onChangePage}
+            pageSize={10}
+            showLessItems={false}
+            showSizeChanger={false}
+          />
+        </div>
+      </div>)}
+
     </div>
   );
 };
