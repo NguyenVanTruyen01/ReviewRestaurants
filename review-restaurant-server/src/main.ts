b52f05117af40bd485716b03ddc8b30ee218032f
NguyenVanTruyen <78594378+NguyenVanTruyen01@users.noreply.github.com>
2023-02-09 13:44:46 +0700

    Update main.ts

--- review-restaurant-server/src/main.ts
+++ review-restaurant-server/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
   app.enableCors(
   {
-    "origin": "http://localhost:3000",
+    "origin": "https://review-restaurants.vercel.app",
       "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
       "preflightContinue": false,
       "optionsSuccessStatus": 204
