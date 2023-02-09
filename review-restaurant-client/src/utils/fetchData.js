72f1c23c6f862fedbd0db6a3935325681606b450
NguyenVanTruyen <78594378+NguyenVanTruyen01@users.noreply.github.com>
2023-02-09 12:46:23 +0700

    Update fetchData.js
    oke

--- review-restaurant-client/src/utils/fetchData.js
+++ review-restaurant-client/src/utils/fetchData.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API = axios.create({baseURL: "http://localhost:5000"})
+const API = axios.create({baseURL: "https://be-review-restaurant.onrender.com"})
 
 export const getDataAPI = async (url, token) => {
     const res = await API.get(`/${url}`, {
